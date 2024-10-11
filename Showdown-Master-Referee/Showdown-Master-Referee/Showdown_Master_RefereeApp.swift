12a354a4c4cb678f4bbafa6a9d107f95d5366dc0
Nicolas HECKER <hecker.nicolas.pro@gmail.com>
2024-10-11 09:31:11 +0200

    Initial Commit

---
+++ Showdown-Master-Referee/Showdown-Master-Referee/Showdown_Master_RefereeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Showdown_Master_RefereeApp.swift
+//  Showdown-Master-Referee
+//
+//  Created by Nicolas Hecker on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Showdown_Master_RefereeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
