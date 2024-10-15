cc5b83dcd360057ddcc0ec5c7b6b9f16f7e72b4e
Nicolas HECKER <hecker.nicolas.pro@gmail.com>
2024-10-15 20:47:07 +0200

    Add State for player one player two and other

--- Showdown-Master-Referee/Showdown-Master-Referee/View/Home.swift
+++ Showdown-Master-Referee/Showdown-Master-Referee/View/Home.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct Home: View {
+    @State var selectedPicker: Int = 0
+    @State var playerOneName: String = ""
+    @State var playerTwoName: String = ""
+    @State var numberOfSetInt: Int = 0
+    @State var pointsPerSetInt: Int = 0
+    @State var numberOfServiceInt: Int = 0
+    @State var changeSide: Bool = true
+    let pickerLabels: [String] = ["1","3", "5", "Team", "Custom"]
     var body: some View {
         ZStack {
             Color.red.opacity(0.3)
@@ -15,28 +23,125 @@ struct Home: View {
             VStack {
                 Text("Player One")
                 
-                TextField("Player one name", text: .constant(""))
+                TextField("Player one name", text: $playerOneName)
+                    .textFieldStyle(.roundedBorder)
                     .background(.white)
                     .padding()
-                    .textFieldStyle(.roundedBorder)
                 
                 Text("Player Two")
                 
-                TextField("Player two name", text: .constant(""))
+                TextField("Player two name", text: $playerTwoName)
+                    .textFieldStyle(.roundedBorder)
                     .background(.white)
                     .padding()
-                    .textFieldStyle(.roundedBorder)
                 
                 Text("Best Of")
                 
-                Picker(selection: .constant(1), label: Text("Best Of")) {
-                    Text("1").tag(1)
-                    Text("3").tag(2)
-                    Text("5").tag(3)
-                    Text("Team").tag(4)
-                    Text("Custom").tag(5)
+                Picker(selection: $selectedPicker, label: Text("Best Of")) {
+                    ForEach(0 ..< pickerLabels.count, id: \.self) { index in
+                        Text(pickerLabels[index]).tag(index)
+                    }
                 }
                 .pickerStyle(.segmented)
+                .padding()
+                
+                Divider()
+                
+                HStack {
+                    Text("Number of Sets")
+                        .font(.headline)
+                    
+                    HStack {
+                        Button(action: {
+                            if numberOfSetInt > 0 { // Limiter la valeur à un minimum de 0
+                                numberOfSetInt -= 1
+                            }
+                        }) {
+                            Image(systemName: "arrowshape.left") // Icône pour "down"
+                                .font(.title)
+                                .padding()
+                        }
+                        
+                        Text("\(numberOfSetInt)")
+                            .font(.title2)
+                            .padding()
+                        
+                        Button(action: {
+                            numberOfSetInt += 1 // Incrémenter la valeur
+                        }) {
+                            Image(systemName: "arrowshape.right") // Icône pour "up"
+                                .font(.title)
+                                .padding()
+                        }
+                    }
+                }
+                
+                HStack {
+                    Text("Points per Set")
+                        .font(.headline)
+                    
+                    HStack {
+                        Button(action: {
+                            if pointsPerSetInt > 0 {
+                                pointsPerSetInt -= 1
+                            }
+                        }) {
+                            Image(systemName: "arrowshape.left")
+                                .font(.title)
+                                .padding()
+                        }
+                        
+                        Text("\(pointsPerSetInt)")
+                            .font(.title2)
+                            .padding()
+                        
+                        Button(action: {
+                            pointsPerSetInt += 1
+                        }) {
+                            Image(systemName: "arrowshape.right")
+                                .font(.title)
+                                .padding()
+                        }
+                    }
+                }
+                
+                HStack {
+                    Text("Number of Service")
+                        .font(.headline)
+                    
+                    HStack {
+                        Button(action: {
+                            if numberOfServiceInt > 0 {
+                                numberOfServiceInt -= 1
+                            }
+                        }) {
+                            Image(systemName: "arrowshape.left")
+                                .font(.title)
+                                .padding()
+                        }
+                        
+                        Text("\(numberOfServiceInt)")
+                            .font(.title2)
+                            .padding()
+                        
+                        Button(action: {
+                            numberOfServiceInt += 1
+                        }) {
+                            Image(systemName: "arrowshape.right")
+                                .font(.title)
+                                .padding()
+                        }
+                    }
+                }
+                
+                HStack {
+                    Text("Change Side")
+                        .font(.headline)
+                    
+                    Toggle(isOn: $changeSide) {
+                    
+                    }
+                }
                 .padding()
             }
         }
