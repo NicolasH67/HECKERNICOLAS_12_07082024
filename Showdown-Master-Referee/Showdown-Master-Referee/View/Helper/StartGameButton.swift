05e2429054571e8c8074eb22952e4ff1d3b31d15
Nicolas HECKER <hecker.nicolas.pro@gmail.com>
2024-12-05 09:18:21 +0100

    Refactor : add StartGameButton files

---
+++ Showdown-Master-Referee/Showdown-Master-Referee/View/Helper/StartGameButton.swift
@@ -0,0 +1,18 @@
+//
+//  StartGameButton.swift
+//  Showdown-Master-Referee
+//
+//  Created by Nicolas Hecker on 04/12/2024.
+//
+
+import SwiftUI
+
+struct StartGameButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartGameButton()
+}
