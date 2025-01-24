024fb81c9396a5ecc6b144e3bc1344c918544dd6
Nicolas HECKER <hecker.nicolas.pro@gmail.com>
2025-01-24 20:51:18 +0100

    add config.swift

---
+++ Showdown-Master-Referee/Showdown-Master-Referee/Service/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  Showdown-Master-Referee
+//
+//  Created by Nicolas Hecker on 24/01/2025.
+//
+
+import Foundation
+
+struct Config {
+    let baseUrl = "https://wpjudvkeaiohzmijckyl.supabase.co/rest/v1"
+    let apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwanVkdmtlYWlvaHptaWpja3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyODE3NzYsImV4cCI6MjA0Nzg1Nzc3Nn0.y7CBBrm8jNydpgEE2L6m6KRu1ilCq_I0O5HmjHBwRIM"
+}
